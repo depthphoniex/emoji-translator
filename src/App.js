0ccb6e5b6d1e0d4cff410a2c756482a493d99879
depthphoniex <88782363+depthphoniex@users.noreply.github.com>
2021-09-28 22:08:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+// declaration of dictionary
+var emojidictionry = {
+  "😀": "Giring face",
+  "😊": "smile",
+  "💘": "heart",
+  "💌": "love letter",
+  "🥺": "pleading face",
+  "🔥": "fire",
+  "✔️": "check mark",
+  "😢": "crying face",
+  "😔": "sad face",
+  "☮️": "peace",
+  "😕": "confused",
+  "😨": "fearful face",
+  "😠": "angry",
+  "🕧": "twelve-thirty",
+  "✌️": "victory hand",
+  "🙌": "rasing hands"
+};
+
+// declaration of emojilist for printing dictionry value
+var emojilist = Object.keys(emojidictionry);
+// processing part
+export default function App() {
+  // calling of usestate function
+  var [translator, settranslator] = useState("");
+  function emojiinputhandler(event) {
+    var userinput = event.target.value;
+    var translator = emojidictionry[userinput];
+    if (translator === undefined) {
+      translator = "please check the input";
+    }
+    // set the value of settranslartor
+    settranslator(translator);
+  }
+  function emojiclickhandler(emoji) {
+    var translator = emojidictionry[emoji];
+    settranslator(translator);
+  }
+  return (
+    <div className="App">
+      <h1>emoji translator</h1>
+      {/* taking input in input area */}
+      <input onChange={emojiinputhandler}></input>
+      {/* output part */}
+      <h2 style={{ textTransform: "capitalize" }}>{translator}</h2>
+      <h3>emoji list</h3>
+      {/* print the emoji in emoji dictionary */}
+      {emojilist.map(function (emoji) {
+        return (
+          <h4
+            onClick={() => emojiclickhandler(emoji)}
+            style={{
+              // styling part
+              display: "inline",
+              fontSize: "25px",
+              padding: "5px 0px 5px 0px",
+              margin: "0px 35px 0px 35px",
+              cursor: "pointer",
+              lineHeight: "3"
+            }}
+            key={emoji}
+          >
+            {emoji}
+          </h4>
+        );
+      })}
+    </div>
+  );
+}
